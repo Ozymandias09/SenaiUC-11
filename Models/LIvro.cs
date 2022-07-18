fd7bb97a28340fd7b6ac4b2b2d6e6bf1df3dc96f
Marcos Batista <batista.vinicius09@gmail.com>
2022-07-17 22:34:06 -0300

    CRUD que fiz durante as aulas do UC 11 Senai
    Crud feito nas aulas do Senai Sp, UC 11

---
+++ Models/LIvro.cs
@@ -0,0 +1,23 @@
+﻿using Chapter.Contexts;
+using Chapter.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+//Add web services to the container
+builder.Services.AddControllers();
+
+builder.Services.AddScoped<ChapterContext, ChapterContext>();
+
+builder.Services.AddTransient<LivroRepository, LivroRepository>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }    
+        public int QtdPaginas { get; set; }
+        public bool Disponivel { get; set; }
+    }
+}
